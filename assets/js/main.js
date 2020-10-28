6083298902be0d0df48a113277283c3303a66e9c
mgmoldova <platonvadim@ya.ru>
2020-10-28 17:26:44 +0200

    added realisation of ADD TO CART

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+let cartCounterLabel = document.querySelector('#cart-counter');
+let buttonsContainer = document.querySelector('#content-container');
+
+let cartCounter = 0;
+let cartPrice = 0;
+
+let btnClickHandler = (e) => {
+    let target = e.target;
+
+    if(target.classList.contains('item-actions__cart')) {
+
+        cartCounterLabel.innerHTML = ++cartCounter;
+        if(cartCounter === 1) cartCounterLabel.style.display = 'block';
+        
+        const mockData = +target.parentElement.previousElementSibling.innerHTML.replace(
+            /^\$(\d+)\s\D+(\d+).*$/u, '$1.$2')
+            cartPrice = Math.round((cartPrice + mockData) * 100) / 100;
+            let restoreHTML = target.innerHTML;
+
+            target.innerHTML = `Added ${cartPrice.toFixed(2)}$`;
+            target.disabled = true;
+            buttonsContainer.removeEventListener('click', btnClickHandler);
+            setTimeout(()=> {
+                target.innerHTML = restoreHTML;
+                target.disabled = false;
+                buttonsContainer.addEventListener('click', btnClickHandler);
+            }, 2000);
+    }
+    
+}
+buttonsContainer.addEventListener('click', btnClickHandler);
